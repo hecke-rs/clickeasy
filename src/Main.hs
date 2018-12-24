265f809b4a20ea04625f79af37fa85ac622433eb
Erin Moon <erin@hashbang.sh>
2018-12-24 16:19:29 -0600

    add blaze-html, build submit form at index route

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# HLINT ignore "Redundant do" #-}
 module Main where
 
 import Web.Scotty
+
 import Network.Wai.Middleware.RequestLogger
+
+import qualified Text.Blaze.Html5 as H
+import Text.Blaze.Html5 ((!))
+import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html.Renderer.Text (renderHtml)
 
 main :: IO ()
 main = do
+  putStrLn "starting server..."
   scotty 3000 $ do
     middleware logStdoutDev
 
-    get "/hello" $ do
-      text "hello world!"
+    get "/" $ do
+      html $ renderHtml
+        $ H.docTypeHtml $ do
+            H.body $ do
+              H.form ! method "post"
+                     ! action "/shorten" $ do
+                H.input ! type_ "text" ! name "url"
+                H.input ! type_ "submit"
