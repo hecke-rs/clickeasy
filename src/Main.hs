3a4b0e5aa9236414c8d54a3ec4676c690637982d
Erin Moon <erin@hashbang.sh>
2018-12-24 15:28:36 -0600

    add wai-extra, register stdout log middleware

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,12 @@
 module Main where
 
 import Web.Scotty
+import Network.Wai.Middleware.RequestLogger
 
 main :: IO ()
 main = do
   scotty 3000 $ do
+    middleware logStdoutDev
+
     get "/hello" $ do
       text "hello world!"
