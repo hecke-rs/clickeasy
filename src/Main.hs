f5c8e8f5a79d2852742a169a8f934c831c6a4f40
Erin Moon <erin@hashbang.sh>
2018-12-24 15:10:43 -0600

    add scotty, hello world route

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
+import Web.Scotty
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  scotty 3000 $ do
+    get "/hello" $ do
+      text "hello world!"
